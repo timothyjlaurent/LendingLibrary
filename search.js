d97129632b360318fff7cc8ddb35ec255f4747f6
Timothy Laurent <laurentt@flip3.engr.oregonstate.edu>
2013-08-18 12:14:30 -0700

    rebuilt search interface

--- search.js
+++ search.js
@@ -6,28 +6,23 @@
  function getSearchOptions() {
 	// Activate jquery form validate
 	$("#searchform").validate();
-	
+	console.log("in getSearchOptions_ debug test");
 	// Call Ajax to retrieve list of item types
 	$.ajax({url: "itemtype.php",
 		type: "GET",
 		dataType: "json",
+		// success: alert("success"),
+		// error: alert("Error")});
 		success: createItemMenu,
 		error: errorQuery});
-	
+			
 	
 	// Watch for item being selected and call Ajax
 	// on item details
 	$("#typeoptions").change(function() {
 		// First clear out prior menu items
-		$("#attrib1opt").empty();
-		$("#attrib1opt").append("<option value=\"\">choose</option>");
-		$("#attrib2opt").empty();
-		$("#attrib2opt").append("<option value=\"\">choose</option>");
-		$("#attrib3opt").empty();
-		$("#attrib3opt").append("<option value=\"\">choose</option>");
-		
+		$("#typefields").empty();
 		// *** POSSIBLE TO ADD A LOCAL CACHE FOR SPEED ***
-		
 		// Now call ajax to get new attribute information
 		$.ajax({url: "attribute.php",
 			type: "GET",
@@ -41,133 +36,49 @@
  
  // Takes JSON returned from AJAX to populate item type menu
  function createItemMenu(data, status) {
-	alert("success");
+	console.log("CreateMenu AJAX Success");
 	var i;
 	for (i = 0; i < data.length; i++) {
+		console.log(data[i]);
 		$("#typeoptions").append("<option value=\"" + data[i] + "\">" + data[i] + "</option>");
 	}
  }
  
  // Prints error message if AJAX call fail
  function errorQuery() {
+		console.log("CreateItem AJAX Success");
 		$("#usermsg").append("Unable to access database");
  }
  
- 
+
+
   // Takes JSON returned from AJAX to populate attribute menus
  function createAttributes(data, status) {
 	// Create key - value associative array
 	var units = new Object();
 	var isNumber = new Object();
-	
-	for (var i = 0; i < data.length; i++) {
-	
-				
+	$('fieldtype').empty();
+	for (var i = 0; i < data.length; i++) {	
 		// Add value to associative array
+		console.log(data[i].field);
 		units[data[i].field] = data[i].units;
 		isNumber[data[i].field] = data[i].numeric;
+		var contID = "container-"+data[i].field;
+		var fldname = "fldname-"+data[i].field;
+		var valname = "val-"+data[i].field;
+		console.log(contID);
+		$('#typefields').append('<div class="container" id="'+contID+'">');
+		$('#'+contID).append('<div id="'+fldname+'" class="col-lg-2 col-sm-2 col-12 control-label">');
+		$('#'+fldname).append('<label>'+data[i].field+'</label>');
+		if (data[i].numeric == 1){
+			$('#'+contID).append('<div class="col-lg-2 col-sm-2 col-12"><select name="comp-'+data[i].field+'" class="form-control"><option value="gt">&gt</option><option value="eq">=</option><option value="lt">&lt</option>');
+		} else {
+			// hides the div when screen is small
+			$('#'+contID).append('<div class="col-lg-2 col-sm-2 col-12 hidden-xs">');
+		}
+		$('#'+contID).append('<div class="col-lg-8 col-sm-8 col-12"><input type="text" id="'+valname+'" class="form-control"></input>');
 		
-		// Add options to 3 attribute menus
-		$("#attrib1opt").append("<option value=\"" + data[i].field + "\">" +data[i].field + "</option>");
-		$("#attrib2opt").append("<option value=\"" + data[i].field + "\">" + data[i].field + "</option>");
-		$("#attrib3opt").append("<option value=\"" + data[i].field + "\">" + data[i].field + "</option>");
 	}
-	
-	// Listen to see if any of the 3 menus are selected
-	// Attribute 1
-	$("#attrib1opt").change(function() {
-		var unitType = units[$(this).val()];
-		var nbrType = isNumber[$(this).val()];
-		
-		// Reset comparison drop down
-		$("#attrib1comp").empty();
-		
-		// Update form input based on data type
-		if (nbrType == 1) {
-			$("#attrib1input").attr("type", "number");
-			$("#attrib1input").attr("placeholder", unitType);
-			$("#attrib1input").attr("required", "true");
-			$("#attrib1comp").append("<option value=\"=\">=</option>");
-			$("#attrib1comp").append("<option value=\">\">></option>");
-			$("#attrib1comp").append("<option value=\"?=\">>=</option>");
-			$("#attrib1comp").append("<option value=\"<\"><</option>");
-			$("#attrib1comp").append("<option value=\"<=\"><=</option>");
-			
-		} else if (nbrType && nbrType == 0) {
-			$("#attrib1input").attr("type", "text");
-			$("#attrib1input").attr("placeholder", "search");
-			$("#attrib1input").attr("required", "true");
-			$("#attrib1comp").append("<option value=\"=\">=</option>");
-		// If "choose" default option selected
-		} else {
-			$("#attrib1input").attr("type", "");
-			$("#attrib1input").attr("placeholder", "");
-			$("#attrib1input").attr("required", "false");
-		}
-	});
-
-	// Attribute 2
-	$("#attrib2opt").change(function() {
-		var unitType = units[$(this).val()];
-		var nbrType = isNumber[$(this).val()];
-		
-		// Reset comparison drop down
-		$("#attrib2comp").empty();
-		
-		// Update form input based on data type
-		if (nbrType == 1) {
-			$("#attrib2input").attr("type", "number");
-			$("#attrib2input").attr("placeholder", unitType);
-			$("#attrib2input").attr("required", "true");
-			$("#attrib2comp").append("<option value=\"=\">=</option>");
-			$("#attrib2comp").append("<option value=\">\">></option>");
-			$("#attrib2comp").append("<option value=\"?=\">>=</option>");
-			$("#attrib2comp").append("<option value=\"<\"><</option>");
-			$("#attrib2comp").append("<option value=\"<=\"><=</option>");
-			
-		} else if (nbrType && nbrType == 0) {
-			$("#attrib2input").attr("type", "text");
-			$("#attrib2input").attr("placeholder", "search");
-			$("#attrib2input").attr("required", "true");
-			$("#attrib2comp").append("<option value=\"=\">=</option>");
-		// If "choose" default option selected
-		} else {
-			$("#attrib2input").attr("type", "");
-			$("#attrib2input").attr("placeholder", "");
-			$("#attrib2input").attr("required", "false");
-		}
-	});
-	// Attribute 3
-	$("#attrib3opt").change(function() {
-		var unitType = units[$(this).val()];
-		var nbrType = isNumber[$(this).val()];
-		
-		// Reset comparison drop down
-		$("#attrib3comp").empty();
-		
-		// Update form input based on data type
-		if (nbrType == 1) {
-			$("#attrib3input").attr("type", "number");
-			$("#attrib3input").attr("placeholder", unitType);
-			$("#attrib3input").attr("required", "true");
-			$("#attrib3comp").append("<option value=\"=\">=</option>");
-			$("#attrib3comp").append("<option value=\">\">></option>");
-			$("#attrib3comp").append("<option value=\"?=\">>=</option>");
-			$("#attrib3comp").append("<option value=\"<\"><</option>");
-			$("#attrib3comp").append("<option value=\"<=\"><=</option>");
-			
-		} else if (nbrType && nbrType == 0) {
-			$("#attrib3input").attr("type", "text");
-			$("#attrib3input").attr("placeholder", "search");
-			$("#attrib3input").attr("required", "true");
-			$("#attrib3comp").append("<option value=\"=\">=</option>");
-		// If "choose" default option selected
-		} else {
-			$("#attrib3input").attr("type", "");
-			$("#attrib3input").attr("placeholder", "");
-			$("#attrib3input").attr("required", "false");
-		}
-	});
 
 }
 
--- sqlconnect.php
+++ sqlconnect.php
@@ -4,12 +4,14 @@ $dbhost = 'oniddb.cws.oregonstate.edu';
 $dbname = 'laurentt-db';
 $dbuser = 'laurentt-db';
 $dbpass = '9IzmnmSrzfhF3SCb';
+    //Report all PHP errors
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    //Connect to database
+    $mysqli = new mysqli($dbhost,$dbuser, $dbpass , $dbname);
 
-$mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Error " . mysqli_error($mysqli));;
-
-
-
-echo 'Successfully connected to database!';
-
-
+    /* check connection */
+    if ($mysqli->connect_errno) {
+        echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+    }
 ?>
